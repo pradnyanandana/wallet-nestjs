836db52bdabee38ebfceb3ad3e576f9fd5549923
Pradnyanandana Suwitra <pradnyanandana@gmail.com>
2023-06-25 20:45:10 +0800

    encrypt code editor

--- views/js/encrypt.js
+++ views/js/encrypt.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   if (typeof window.ethereum !== 'undefined') {
     var addressPlaceholder = document.querySelector('#account-address');
     var connectButton = document.querySelector('button.connect');
+    var actions = document.querySelector('.actions');
+    var actionEncrypt = actions.querySelector('button.encrypt');
+    var actionDecrypt = actions.querySelector('button.decrypt');
 
     addressPlaceholder.innerHTML = 'loading...';
 
@@ -11,13 +14,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
       if (!accounts[0]) {
         connectButton.setAttribute('style', 'display:block');
+      } else {
+        actions.setAttribute('style', 'display:flex');
       }
     });
 
     connectButton.addEventListener('click', function () {
       window.ethereum
         .enable()
-        .then(() => {
+        .then(function () {
           window.location.reload();
         })
         .catch(() => {
@@ -25,9 +30,27 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    window.ethereum.on('accountsChanged', () => window.location.reload());
-    window.ethereum.on('disconnect', () => window.location.reload());
+    window.ethereum.on('accountsChanged', function () {
+      window.location.reload();
+    });
+
+    window.ethereum.on('disconnect', function () {
+      window.location.reload();
+    });
   } else {
     alert('Metamask is not installed or not available');
   }
+
+  require.config({
+    paths: {
+      vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs',
+    },
+  });
+  require(['vs/editor/editor.main'], () => {
+    monaco.editor.create(document.getElementById('editor'), {
+      value: '',
+      language: 'javascripts',
+      theme: 'vs-dark',
+    });
+  });
 });
